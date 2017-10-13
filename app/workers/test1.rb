ec24b02a2cc0796bd32c104995fdd2b0112ea7ce
André Rodrigues <a.andremr@gmail.com>
2017-10-12 22:01:23 -0300

    publish event manually test

--- app/workers/test1.rb
+++ app/workers/test1.rb
@@ -14,4 +14,16 @@ class Test1Worker
     arguments: { 'x-dead-letter-exchange': "#{QUEUE_NAME}-retry" },
     retry_max_times: 4,
     retry_timeout: 15.minutes.in_milliseconds
+
+  ALLOWED_KEYS = [
+    :ble,
+    :bla
+  ]
+
+  def work(message)
+    RabbitmqUtils::MessagePublisher.instance.publish!(
+      {}.to_json,
+      routing_key: 'test-publisher.routing_key'
+    )
+  end
 end
